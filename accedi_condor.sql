80e86f3ac391f00912a5c21f842e1b879def8775
t3rzo <castaldomichele44@gmail.com>
2026-05-27 12:08:15 +0200

    Versione 27 maggio 2026 2

--- accedi_condor.sql
+++ accedi_condor.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Mag 27, 2026 alle 11:35
+-- Creato il: Mag 27, 2026 alle 12:07
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,46 +28,48 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `atleti` (
-  `numero_tesseramento` varchar(50) NOT NULL PRIMARY KEY,
   `nome` varchar(50) NOT NULL,
   `cognome` varchar(50) NOT NULL,
-  `turno` varchar(20) NOT NULL,
+  `turno` int(11) NOT NULL,
+  `numero_tesseramento` varchar(50) NOT NULL,
   `grado_cintura` varchar(50) NOT NULL,
-  `data_nascita` date,
-  `telefono` varchar(20),
-  `email` varchar(100)
+  `data_nascita` date DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `atleti`
 --
 
-INSERT INTO `atleti` (`numero_tesseramento`, `nome`, `cognome`, `turno`, `grado_cintura`, `data_nascita`, `telefono`, `email`) VALUES
-('107500', 'Michele', 'Castaldo', '3', 'Nera 1ºDan', '1986-11-16', '3423489820', 'castaldo.michele@itsdallachiesa.edu.it'),
-('156144', 'Luigi', 'Piscopo', '3', 'Nera 3ºDan', NULL, NULL, NULL),
-('164383', 'Paolo', 'Testa', '3', 'Nera 1ºDan', NULL, NULL, NULL),
-('172614', 'Francesco Pio', 'Calvanese', '3', 'Nera 2ºDan', NULL, NULL, NULL),
-('174642', 'Fabio', 'Laudando', '3', 'Nera 1ºDan', NULL, NULL, NULL),
-('174644', 'Simone', 'Ferrara', '3', 'Nera 1ºDan', NULL, NULL, NULL),
-('176295', 'Ciro', 'Addevico', '3', 'Rossa', NULL, NULL, NULL),
-('185253', 'Luigi', 'Tuccillo', '3', 'Nera 1ºDan', NULL, NULL, NULL),
-('186734', 'Ciro', 'Maiello', '2', 'Verde', NULL, NULL, NULL),
-('192586', 'Vittorio Maria', 'Guerra', '3', 'Nera 1ºDan', NULL, NULL, NULL),
-('194332', 'Alessandro', 'Allocca', '3', 'Nera', NULL, NULL, NULL),
-('202464', 'Emanuele', 'Sainas', '2', 'Blu', NULL, NULL, NULL),
-('203025', 'Carlo', 'Serbati', '2', 'Blu', NULL, NULL, NULL),
-('206609', 'Antonio', 'Addevico', '2', 'Verde', NULL, NULL, NULL),
-('207424', 'Raffaele', 'Ortoglio', '2', 'Verde', NULL, NULL, NULL),
-('217749', 'Domenico', 'Laezza', '2', 'Verde', NULL, NULL, NULL),
-('222049', 'Mario', 'De Riso', '2', 'Verde', NULL, NULL, NULL),
-('222624', 'Gioia Nunzia', 'Perrotta', '3', 'Verde', NULL, NULL, NULL),
-('227423', 'Alessandro', 'Sole', '2', 'Gialla', NULL, NULL, NULL),
-('227974', 'Francesca Pia', 'Duro', '3', 'Verde', NULL, NULL, NULL),
-('235507', 'Luigia', 'De Luca', '3', 'Verde', NULL, NULL, NULL),
-('238110', 'Matteo', 'Colindo', '1', 'Bianca', NULL, NULL, NULL),
-('243660', 'Diego', 'Caropreso', '1', 'Bianca', NULL, NULL, NULL),
-('250757', 'Francesco', 'Testa', '2', 'Blu', NULL, NULL, NULL),
-('911', 'Pacifico', 'Laezza', '4', 'Nera 7°Dan', NULL, NULL, NULL);
+INSERT INTO `atleti` (`nome`, `cognome`, `turno`, `numero_tesseramento`, `grado_cintura`, `data_nascita`, `telefono`, `email`) VALUES
+('Michele', 'Castaldo', 3, '107500', 'Nera 1ºDan', '0206-11-16', '423489820', 'castaldo.michele@itsdallachiesa.edu.it'),
+('Luigi', 'Piscopo', 3, '156144', 'Nera 3ºDan', NULL, NULL, NULL),
+('Paolo', 'Testa', 3, '164383', 'Nera 1ºDan', NULL, NULL, NULL),
+('Francesco Pio', 'Calvanese', 3, '172614', 'Nera 2ºDan', NULL, NULL, NULL),
+('Fabio', 'Laudando', 3, '174642', 'Nera 1ºDan', NULL, NULL, NULL),
+('Simone', 'Ferrara', 3, '174644', 'Nera 1ºDan', NULL, NULL, NULL),
+('Ciro', 'Addevico', 3, '176295', 'Rossa', NULL, NULL, NULL),
+('Luigi', 'Tuccillo', 3, '185253', 'Nera 1ºDan', NULL, NULL, NULL),
+('Ciro', 'Maiello', 2, '186734', 'Verde', NULL, NULL, NULL),
+('Vittorio Maria', 'Guerra', 3, '192586', 'Nera 1ºDan', NULL, NULL, NULL),
+('Alessandro', 'Allocca', 3, '194332', 'Nera', NULL, NULL, NULL),
+('Emanuele', 'Sainas', 2, '202464', 'Blu', NULL, NULL, NULL),
+('Carlo', 'Serbati', 2, '203025', 'Blu', NULL, NULL, NULL),
+('Antonio', 'Addevico', 2, '206609', 'Verde', NULL, NULL, NULL),
+('Raffaele', 'Ortoglio', 2, '207424', 'Verde', NULL, NULL, NULL),
+('Domenico', 'Laezza', 2, '217749', 'Verde', NULL, NULL, NULL),
+('Mario', 'De Riso', 2, '222049', 'Verde', NULL, NULL, NULL),
+('Gioia Nunzia', 'Perrotta', 3, '222624', 'Verde', NULL, NULL, NULL),
+('Alessandro', 'Sole', 2, '227423', 'Gialla', NULL, NULL, NULL),
+('Francesca Pia', 'Duro', 3, '227974', 'Verde', NULL, NULL, NULL),
+('Luigia', 'De Luca', 3, '235507', 'Verde', NULL, NULL, NULL),
+('Matteo', 'Colindo', 1, '238110', 'Bianca', NULL, NULL, NULL),
+('Diego', 'Caropreso', 1, '243660', 'Bianca', NULL, NULL, NULL),
+('Francesco', 'Testa', 2, '250757', 'Blu', NULL, NULL, NULL),
+('Test', 'Test', 1, '71315806', 'Bianca', NULL, NULL, NULL),
+('test1', 'test', 1, '72905261', 'Bianca', NULL, NULL, NULL),
+('Pacifico', 'Laezza', 4, '911', 'Nera 7°Dan', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -75,47 +78,54 @@ INSERT INTO `atleti` (`numero_tesseramento`, `nome`, `cognome`, `turno`, `grado_
 --
 
 CREATE TABLE `credenziali` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `utente` varchar(50) NOT NULL UNIQUE,
+  `id` int(11) NOT NULL,
+  `utente` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `numero_tesseramento` varchar(50) NOT NULL,
-  FOREIGN KEY (`numero_tesseramento`) REFERENCES `atleti` (`numero_tesseramento`) ON DELETE CASCADE
+  `numero_tesseramento` varchar(50) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cognome` varchar(50) DEFAULT NULL,
+  `data_nascita` date DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `credenziali`
 --
 
-INSERT INTO `credenziali` (`id`, `utente`, `password`, `numero_tesseramento`) VALUES
-(6, 'ccc', 'ccc', '107500');
+INSERT INTO `credenziali` (`id`, `utente`, `password`, `numero_tesseramento`, `nome`, `cognome`, `data_nascita`, `telefono`, `email`) VALUES
+(2, 'sdsd', '123', NULL, 'm', 'c', '1212-03-21', '233', 'srer@gmail.com'),
+(5, 'amico2', 'ste', NULL, 'stepan', 'maksimovic', '2007-03-01', '3275805522', 'stepanmaksyimiv@gmail.com'),
+(6, 'ccc', 'ccc', '107500', 'MICHELE', 'CASTALDO', '0206-11-16', '423489820', 'castaldo.michele@itsdallachiesa.edu.it');
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `corsi`
+-- Struttura della tabella `gara_iscrizioni`
 --
 
-CREATE TABLE `corsi` (
-  `id_corso` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nome` varchar(100) NOT NULL,
-  `turno` varchar(20) NOT NULL
+CREATE TABLE `gara_iscrizioni` (
+  `id` int(11) NOT NULL,
+  `id_gara` int(11) NOT NULL,
+  `numero_tesseramento` varchar(50) DEFAULT NULL,
+  `nome_atleta` varchar(100) DEFAULT NULL,
+  `allenatore` varchar(100) DEFAULT NULL,
+  `data_iscrizione` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Struttura della tabella `iscrizioni_corsi`
+-- Dump dei dati per la tabella `gara_iscrizioni`
 --
 
-CREATE TABLE `iscrizioni_corsi` (
-  `id_iscrizione` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `numero_tesseramento` varchar(50) NOT NULL,
-  `id_corso` int(11) NOT NULL,
-  `turno` varchar(20),
-  `data_iscrizione` timestamp DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`numero_tesseramento`) REFERENCES `atleti` (`numero_tesseramento`) ON DELETE CASCADE,
-  FOREIGN KEY (`id_corso`) REFERENCES `corsi` (`id_corso`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `gara_iscrizioni` (`id`, `id_gara`, `numero_tesseramento`, `nome_atleta`, `allenatore`, `data_iscrizione`) VALUES
+(1, 101, '194332', 'Allocca Alessandro', 'ertre', '2026-05-06 09:34:14'),
+(2, 101, '172614', 'Calvanese Francesco Pio', 'ertre', '2026-05-06 09:34:14'),
+(3, 105, '172614', 'Calvanese Francesco Pio', 'Pacifico Laezza', '2026-05-21 09:06:41'),
+(4, 105, '243660', 'Caropreso Diego', 'Pacifico Laezza', '2026-05-21 09:06:41'),
+(5, 105, '107500', 'Castaldo Michele', 'Pacifico Laezza', '2026-05-21 09:06:41'),
+(6, 105, '192586', 'Guerra Vittorio Maria', 'Pacifico Laezza', '2026-05-21 09:06:41'),
+(7, 105, '217749', 'Laezza Domenico', 'Pacifico Laezza', '2026-05-21 09:06:41'),
+(8, 105, '156144', 'Piscopo Luigi', 'Pacifico Laezza', '2026-05-21 09:06:41');
 
 -- --------------------------------------------------------
 
@@ -124,7 +134,7 @@ CREATE TABLE `iscrizioni_corsi` (
 --
 
 CREATE TABLE `gare` (
-  `id_gara` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id_gara` int(11) NOT NULL,
   `titolo` varchar(200) NOT NULL,
   `data` varchar(50) NOT NULL,
   `luogo` varchar(150) NOT NULL,
@@ -148,27 +158,103 @@ INSERT INTO `gare` (`id_gara`, `titolo`, `data`, `luogo`, `specialita`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `gara_iscrizioni`
+-- Struttura della tabella `turni`
 --
 
-CREATE TABLE `gara_iscrizioni` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id_gara` int(11) NOT NULL,
-  `numero_tesseramento` varchar(50) NOT NULL,
-  `allenatore` varchar(100),
-  `data_iscrizione` timestamp DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`id_gara`) REFERENCES `gare` (`id_gara`) ON DELETE CASCADE,
-  FOREIGN KEY (`numero_tesseramento`) REFERENCES `atleti` (`numero_tesseramento`) ON DELETE RESTRICT
+CREATE TABLE `turni` (
+  `id_turno` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `descrizione` varchar(100) DEFAULT NULL,
+  `orario` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `gara_iscrizioni`
+-- Dump dei dati per la tabella `turni`
 --
 
-INSERT INTO `gara_iscrizioni` (`id`, `id_gara`, `numero_tesseramento`, `allenatore`, `data_iscrizione`) VALUES
-(1, 101, '194332', 'ertre', '2026-05-06 09:34:14'),
-(2, 101, '172614', 'ertre', '2026-05-06 09:34:14');
+INSERT INTO `turni` (`id_turno`, `nome`, `descrizione`, `orario`) VALUES
+(1, '1° Turno', 'Kids / Cadetti', '17:00 - 18:30'),
+(2, '2° Turno', 'Cadetti / Junior', '18:30 - 20:00'),
+(3, '3° Turno', 'Senior', '20:00 - 21:30'),
+(4, 'Allenatori/Tecnici', 'Maestro', '');
 
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `atleti`
+--
+ALTER TABLE `atleti`
+  ADD PRIMARY KEY (`numero_tesseramento`),
+  ADD KEY `fk_atleti_turni` (`turno`);
+
+--
+-- Indici per le tabelle `credenziali`
+--
+ALTER TABLE `credenziali`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `utente` (`utente`),
+  ADD UNIQUE KEY `credenziali_tesseramento_unique` (`numero_tesseramento`);
+
+--
+-- Indici per le tabelle `gara_iscrizioni`
+--
+ALTER TABLE `gara_iscrizioni`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_gara` (`id_gara`),
+  ADD KEY `gara_iscrizioni_tesseramento_idx` (`numero_tesseramento`);
+
+--
+-- Indici per le tabelle `gare`
+--
+ALTER TABLE `gare`
+  ADD PRIMARY KEY (`id_gara`);
+
+--
+-- Indici per le tabelle `turni`
+--
+ALTER TABLE `turni`
+  ADD PRIMARY KEY (`id_turno`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `credenziali`
+--
+ALTER TABLE `credenziali`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT per la tabella `gara_iscrizioni`
+--
+ALTER TABLE `gara_iscrizioni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `atleti`
+--
+ALTER TABLE `atleti`
+  ADD CONSTRAINT `fk_atleti_turni` FOREIGN KEY (`turno`) REFERENCES `turni` (`id_turno`);
+
+--
+-- Limiti per la tabella `credenziali`
+--
+ALTER TABLE `credenziali`
+  ADD CONSTRAINT `credenziali_atleti_fk` FOREIGN KEY (`numero_tesseramento`) REFERENCES `atleti` (`numero_tesseramento`) ON DELETE CASCADE;
+
+--
+-- Limiti per la tabella `gara_iscrizioni`
+--
+ALTER TABLE `gara_iscrizioni`
+  ADD CONSTRAINT `gara_iscrizioni_atleti_fk` FOREIGN KEY (`numero_tesseramento`) REFERENCES `atleti` (`numero_tesseramento`),
+  ADD CONSTRAINT `gara_iscrizioni_ibfk_1` FOREIGN KEY (`id_gara`) REFERENCES `gare` (`id_gara`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
